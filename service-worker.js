2fc1140a95c1c62e9fc5b155069ad418f830eaea
lsomacarrera-cell <l.somacarrera@gmail.com>
2026-09-01 18:28:30 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="celam-v8-1";
+const FILES=["./","./index.html","./style.css","./data.js","./app.js","./manifest.webmanifest","./assets/celam-logo.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
